827187d33180ddac9808f362ca6a0ec77575fda1
Rahul Dave <rahuldave@gmail.com>
2011-04-01 15:06:09 -0400

    fixed paths in hut-biblist, got rdf loading down

---
+++ mast/loadfiles-hut-obsv.py
@@ -0,0 +1,31 @@
+#loadfiles
+__version__="0.1"
+from pysesame import connection
+from urllib import quote_plus, urlencode, quote
+import os.path, sys
+import uuid
+
+c=connection('http://localhost:8081/openrdf-sesame/')
+c.use_repository('testads4')
+context=None
+testcodeuristart='<http://ads.harvard.edu/sem/context#'
+DATA="../mast_hut-rdf"
+
+#c.addnamespace('fb','http://rdf.freebase.com/ns/')
+#c.addnamespace('dc','http://purl.org/dc/elements/1.1/')
+import os.path, sys, os, glob
+
+identifier=str(uuid.uuid4())+"-"+__file__+"-"+__version__
+
+context=quote_plus(testcodeuristart+identifier+">")
+obsfiles=range(int(sys.argv[2]))
+print obsfiles
+for ele in obsfiles:
+    strele=str(ele+1)
+    filename=DATA+"/"+sys.argv[1]+"."+strele+".rdf"
+    #print filename
+    if os.path.isfile(filename):
+        print filename
+        c.postfile(filename, context)
+    else:
+        "FILE not found: ", filename
---
+++ mast/loadfiles-hut-pub.py
@@ -0,0 +1,31 @@
+#loadfiles
+__version__="0.1"
+from pysesame import connection
+from urllib import quote_plus, urlencode, quote
+import os.path, sys
+import uuid
+
+c=connection('http://localhost:8081/openrdf-sesame/')
+c.use_repository('testads4')
+context=None
+testcodeuristart='<http://ads.harvard.edu/sem/context#'
+DATA="../mast_hut-rdf"
+
+#c.addnamespace('fb','http://rdf.freebase.com/ns/')
+#c.addnamespace('dc','http://purl.org/dc/elements/1.1/')
+import os.path, sys, os, glob
+
+identifier=str(uuid.uuid4())+"-"+__file__+"-"+__version__
+
+context=quote_plus(testcodeuristart+identifier+">")
+filename=DATA+"/"+sys.argv[1]+".rdf"
+if not os.path.exists(filename):
+    print "Path not found: "+filename
+    sys.exit(-1)
+
+
+if os.path.isfile(filename):
+    print filename
+    c.postfile(filename, context)
+else:
+    "FILE not found: ", filename
